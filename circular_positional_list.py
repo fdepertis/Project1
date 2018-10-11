7a6f02b0f4ef137dee346eba6c41cfeedcaa8bf1
Francesco de Pertis <f.depertis@studenti.unisa.it>
2018-10-11 13:57:19 +0200

    Qui funziona tutto
    Maneggiare con cura

--- circular_positional_list.py
+++ circular_positional_list.py
@@ -4,73 +4,30 @@ class CircularPositionalList(PositionalList):
 
     def __init__(self):
         """Create an empty list."""
-        self._header = self._Node(None, None, None)
+        self._header = None
         self._size = 0
-
-    #-------------------------- public accessors --------------------------
 
     def _make_position(self, node):
         """Return Position instance for given node."""
         return self.Position(self, node)  # legitimate position
 
-    """restituisce la Position dell’elemento che è identificato
-     come il primo oppure None se la lista è vuota"""
-
     def first(self):
+        """Restituisce la Position dell’elemento che è identificato
+             come il primo oppure None se la lista è vuota"""
         if self.is_empty():
             return None
         else:
             return self._make_position(self._header)
 
-    """Restituisce la Position dell'elemento che è identificato 
-    come l'ultimo oppure None se la lista è vuota"""
-
     def last(self):
+        """Restituisce la Position dell'elemento che è identificato
+            come l'ultimo oppure None se la lista è vuota"""
         if self.is_empty():
             return None
         else:
             return self._make_position(self._header._prev)
 
-    def __len__(self):
-        """Return the number of elements in the list."""
-        return self._size
-
-    def is_empty(self):
-        """Return True if list is empty."""
-        return self._size == 0
-
-    def _set_header(self, e):
-        self._header = self._Node(e, self._header, self._header)
-        self._header._next = self._header
-        self._header._prev = self._header
-        self._size = 1
-
-    def add_first(self,e):
-        """Inserisce l'elemento e in testa alla lista e restituisce la Position del nuovo elemento"""
-        if self.is_empty():
-            self._set_header(e)
-        else:
-            new_node = self._Node(e, self._header._prev, self._header)
-            self._header._prev._next = new_node
-            self._header._prev = new_node
-            self._header = new_node
-            self._size += 1
-        return self._make_position(self._header)
-
-
-    def add_last(self, e):
-        """Inserisce l'elemento e in coda alla lista e restituisce la Position del nuovo elemento"""
-        if self.is_empty():
-            self._set_header(e)
-        else:
-            new_node = self._Node(e, self._header, self._header._prev)
-            self._header._prev._next = new_node
-            self._header._prev = new_node
-            self._size += 1
-        return self._make_position(self._header._prev)
-
-
-    def before(self,p):
+    def before(self, p):
         """Restituisce l'elemento nella Position precedente a p, None se p non ha un predecessore e
                 ValueError se p non è una position della lista"""
         if self.is_empty():
@@ -78,13 +35,69 @@ class CircularPositionalList(PositionalList):
         node = self._validate(p)
         return self._make_position(node._prev)
 
-    def after(self,p):
+    def after(self, p):
         """Restituisce l'elemento nella Position successiva a p, None se p non ha un successore e
                 ValueError se p non è una position della lista"""
         if self.is_empty():
             return None
         node = self._validate(p)
         return self._make_position(node._next)
+
+    def is_empty(self):
+        """Return True if list is empty."""
+        return self._size == 0
+
+    def __len__(self):
+        """Return the number of elements in the list."""
+        return self._size
+
+    def _set_header(self, e):
+        self._header = self._Node(e, self._header, self._header)
+        self._header._next = self._header
+        self._header._prev = self._header
+        self._size = 1
+        return self._make_position(self._header)
+
+    def add_first(self, e):
+        """Inserisce l'elemento e in testa alla lista e restituisce la Position del nuovo elemento"""
+        if self.is_empty():
+            return self._set_header(e)
+        else:
+            header_position = self.add_before(self.first(), e)
+            self._header = self._validate(header_position)
+            return header_position
+
+    def add_last(self, e):
+        """Inserisce l'elemento e in coda alla lista e restituisce la Position del nuovo elemento"""
+        if self.is_empty():
+            return self._set_header(e)
+        else:
+            return self.add_after(self.last(), e)
+
+    def add_before(self, p, e):
+        """Inserisce un nuovo (elemento e) prima del nodo nella (Position p) e restituisce la position
+                del nuovo elemento"""
+        return super().add_before(p, e)
+
+    def add_after(self, p, e):
+        """Inserisce un nuovo(elemento e) dopo del nodo nella(Position p) e restituisce la position
+                del nuovo elemento """
+        return super().add_after(p, e)
+
+    def find(self, e):
+        """Restituisce una Position contenente la prima occorrenza dell'elemento e nella lista
+                o None se non e' presente"""
+        cursor = self.first()
+        for i in range(self._size):
+            if cursor.element() == e:
+                return cursor
+            else:
+                cursor = self.after(cursor)
+        return None
+
+    def replace(self, p, e):
+        """Sostituisce l'elemento in Position p con l'elemento e e restituisce il vecchio elemento"""
+        return super().replace(p, e)
 
     def copy(self):
         new_list = CircularPositionalList()
@@ -151,3 +164,4 @@ if __name__ == '__main__':
     print("Sia cpl3 = cpl + cpl2")
     cpl3 = cpl + cpl2
     print(str(cpl3))
+    print(cpl.last().element(), cpl.before(cpl.last()).element(),cpl.before(cpl.before(cpl.last())).element())
